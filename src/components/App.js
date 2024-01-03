40ce23aac1e5eed8cabce5408e989de636800f17
kaustavsarkar14 <sarkar.kaustav14@gmail.com>
2024-01-03 10:52:58 +0530

    test

--- src/components/App.js
+++ src/components/App.js
@@ -3,35 +3,28 @@ import "./../styles/App.css";
 
 const Tooltip = ({ text, children }) => {
   const [show, setShow] = useState(false);
-
-  // Check if the child is an h1 and replace it with h2 for test compatibility
-  const wrappedChildren = React.Children.map(children, child => {
-    const Tag = child.type === 'h1' ? 'h2' : child.type;
-
-    return (
-      <Tag 
-        className={`tooltip ${child.props.className || ''}`}
+  return (
+    <div className="tooltip">
+      <div
         onMouseEnter={() => setShow(true)}
         onMouseLeave={() => setShow(false)}
       >
-        {child.props.children}
-        {show && (
-          <div style={{ backgroundColor: "red" }} className="tooltiptext">
-            {text}
-          </div>
-        )}
-      </Tag>
-    );
-  });
-
-  return <>{wrappedChildren}</>;
+        {children}
+      </div>
+      {show && (
+        <div style={{ backgroundColor: "red" }} className="tooltiptext">
+          {text}
+        </div>
+      )}
+    </div>
+  );
 };
 
 const App = () => {
   return (
     <div>
       <Tooltip text="This is a tooltip">
-        <h1>Hover over me</h1> {/* This will be rendered as an h2 */}
+        <h1>Hover over me</h1>
       </Tooltip>
       <Tooltip text="This is another tooltip">
         <p>Hover over me to see another tooltip</p>
