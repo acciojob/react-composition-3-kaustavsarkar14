19e627870f3f32a77cb0cce34d3be3ee8e662389
kaustavsarkar14 <sarkar.kaustav14@gmail.com>
2024-01-03 11:42:44 +0530

    test

--- src/components/App.js
+++ src/components/App.js
@@ -5,10 +5,10 @@ import Tooltip from "./Tooltip";
 const App = () => {
   return (
     <div>
-      <h2>
+      <h2 className="tooltip">
         <Tooltip text="This is a tooltip">Hover over me</Tooltip>
       </h2>
-      <p>
+      <p className="tooltip">
         <Tooltip text="This is another tooltip">
           Hover over me to see another tooltip
         </Tooltip>
